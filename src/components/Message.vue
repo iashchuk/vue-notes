ca827bcc0968362270056978551348d8fc0d8a58
iashchuk <221box@list.ru>
2019-09-26 23:54:37 +0300

    Добавляет компонент Message

---
+++ src/components/Message.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="message">
+    <h1 class="message__text">{{text}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true,
+      default: "field can't be blank"
+    }
+  }
+};
+</script>
+
+<style lang='scss' scoped>
+.message {
+  text-align: center;
+  padding: 20px;
+}
+
+.message__text {
+  color: #ba3838;
+}
+</style>
