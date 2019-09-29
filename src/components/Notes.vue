ae17db07faeaa48ca3ee16421d6ea5cd0d9d11fe
iashchuk <221box@list.ru>
2019-09-29 12:25:07 +0300

    Добавляет компонент Notes

---
+++ src/components/Notes.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="notes">
+    <Note
+      v-for="(note, index) in notes"
+      :key="index"
+      :grid="grid"
+      :title="note.title"
+      :descr="note.descr"
+      :date="note.date"
+      @removeNote="removeNote(index)"
+    />
+  </div>
+</template>
+
+<script>
+import Note from "@/components/Note";
+
+export default {
+  components: {
+    Note
+  },
+  props: {
+    notes: {
+      type: Array,
+      required: true
+    },
+    grid: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    removeNote(id) {
+      this.$emit("removeNote", id);
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.notes {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  padding: 40px 0;
+}
+</style>
