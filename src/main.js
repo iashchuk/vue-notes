aff6b6f1aecf6cb623a02c897501969362d3ae9b
iashchuk <221box@list.ru>
2019-10-06 21:20:13 +0300

    Устанавливает v-click-outside

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
+import vco from "v-click-outside";
 
 import "./assets/scss/main.scss";
+
+Vue.use(vco);
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App)
+    render: h => h(App)
 }).$mount("#app");
