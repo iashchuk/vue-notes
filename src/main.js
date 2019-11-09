daa59f9e3c29fb7d96d0b88e1cc1b65ca8c8b6c7
iashchuk <221box@list.ru>
2019-11-10 01:02:44 +0300

    Устанавливает и настраивает Vuex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import vco from "v-click-outside";
+import store from "./store/store";
 
 import "./assets/scss/main.scss";
 
@@ -9,5 +10,6 @@ Vue.use(vco);
 Vue.config.productionTip = false;
 
 new Vue({
+    store,
     render: h => h(App)
 }).$mount("#app");
